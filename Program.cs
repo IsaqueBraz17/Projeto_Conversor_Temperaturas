93cc80d106af11f9a2bfbbfb27e07d7f9d99cf12
Isaque de Oliveira Braz <157842012+IsaqueBraz17@users.noreply.github.com>
2025-01-25 19:09:29 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApredendoProgramacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Conversão de temperaturas
+
+            Console.WriteLine("### CONVERSOR DE TEMPERATURAS ###");
+            Console.Write("Insira a temperatura em Celcius: ");
+            var celcius = double.Parse(Console.ReadLine());
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Convertendo...");
+            Console.WriteLine();
+
+            // Operação de conversão
+            var fahrenheit = (celcius * 9 / 5) + 32;
+
+            var kelvin = celcius + 273.15;
+
+            Console.WriteLine(celcius + " graus celcius = " + fahrenheit + " graus fahrenheit");
+            Console.WriteLine(celcius + " graus celcius = " + kelvin + " graus kelvin");
+            Console.WriteLine();
+
+            Console.WriteLine("Encerrando...");
+        }
+    }
+}
